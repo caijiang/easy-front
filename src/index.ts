d7977c8d89eb658248e68d8b2e516593e1faa247
Cai Jiang <luffy.ja@gmail.com>
2022-11-16 02:22:33 +0800

    移除一些有问题的函数

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
 export { default as Foo } from './Foo';
 export { default as ChangePasswordForm } from './ChangePasswordForm';
-export * from './common/utils';
-export * from './common/spring/withoutRest';
-export * from './common/message';
